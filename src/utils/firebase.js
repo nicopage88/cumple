5eb27e9d423c8db327c30a1578cf2478b5abc005
NicoPage <nicolas.gonzalez.araneda@ciisa.cl>
2021-03-27 20:59:03 -0300

    first commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwa8rgbIBd9agYiV0D5h_3ZGrP_INu3pw",
+  authDomain: "cumple-a192a.firebaseapp.com",
+  projectId: "cumple-a192a",
+  storageBucket: "cumple-a192a.appspot.com",
+  messagingSenderId: "471217911896",
+  appId: "1:471217911896:web:4f0ac1b267f7c2c3fa322d"
+};
+
+export default firebase.initializeApp(firebaseConfig);
